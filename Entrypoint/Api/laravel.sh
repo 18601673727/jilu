8d36e511d60f83ac727a36f0033d61f2ca2591e4
18601673727 <18601673727@163.com>
2016-08-18 22:11:13 +0800

    updated deployment shell

--- Entrypoint/Api/laravel.sh
+++ Entrypoint/Api/laravel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /application/code
-chmod -R 755 storage bootstrap/cache
+chmod -R 777 storage bootstrap/cache
 composer install --no-plugins --no-scripts
 php artisan migrate
