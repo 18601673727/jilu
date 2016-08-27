c5a19888374f38d5ae6a0838e9190e8c5298d0fa
18601673727 <18601673727@163.com>
2016-08-27 14:10:51 +0800

    dashboard updates, added wechat user info

--- Api/resources/views/welcome.blade.php
+++ Api/resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
 @section('content')
     <div class="nav">
         <img src="image/logo.png" alt="jilu">
-        {{ dd(session('wechat.oauth_user')) }}
     </div>
 
     <div id="stopwatch">
@@ -13,6 +12,10 @@
         <button class="weui_btn weui_btn_primary start-button">走你</button>
         <button class="weui_btn weui_btn_primary record-button" style="display: none;">到位</button>
         <button class="weui_btn weui_btn_warn stop-button" style="display: none;">重来</button>
+        <div class="clearfix">
+            <img class="pull-left" src="session('wechat.oauth_user')->avatar" alt="">
+            <a class="pull-right" href="/events">{{ session('wechat.oauth_user')->nickname }} 的记录</a>
+        </div>
     </div>
 @endsection
 @section('script')
