7044a877d77c7e3bd66265c17460cab840c41474
18601673727 <18601673727@163.com>
2016-08-29 03:50:43 +0800

    timeline added

--- Api/resources/views/event/list.blade.php
+++ Api/resources/views/event/list.blade.php
@@ -3,18 +3,16 @@
 @section('title', session('wechat.oauth_user')->nickname .'的记录')
 
 @section('content')
-    <div class="text-left">
-        <div class="weui_cells_title">我的记录</div>
-        <div class="weui_cells">
-            @foreach($data as $row)
-                <div class="weui_cell">
-                    <div class="weui_cell_hd"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC4AAAAuCAMAAABgZ9sFAAAAVFBMVEXx8fHMzMzr6+vn5+fv7+/t7e3d3d2+vr7W1tbHx8eysrKdnZ3p6enk5OTR0dG7u7u3t7ejo6PY2Njh4eHf39/T09PExMSvr6+goKCqqqqnp6e4uLgcLY/OAAAAnklEQVRIx+3RSRLDIAxE0QYhAbGZPNu5/z0zrXHiqiz5W72FqhqtVuuXAl3iOV7iPV/iSsAqZa9BS7YOmMXnNNX4TWGxRMn3R6SxRNgy0bzXOW8EBO8SAClsPdB3psqlvG+Lw7ONXg/pTld52BjgSSkA3PV2OOemjIDcZQWgVvONw60q7sIpR38EnHPSMDQ4MjDjLPozhAkGrVbr/z0ANjAF4AcbXmYAAAAASUVORK5CYII=" alt="" style="width:20px;margin-right:5px;display:block"></div>
-                    <div class="weui_cell_bd weui_cell_primary">
-                        <p>{{ round(($row->ended_at - $row->started_at) / 1000, 1) }} 秒, {{ round($row->score) }} 分</p>
-                    </div>
-                    <div class="weui_cell_ft">{{ $row->created_at }}</div>
+    <ul class="cbp_tmtimeline">
+        @foreach($data as $row)
+            <li>
+                <time class="cbp_tmtime" datetime="{{ $row->created_at }}"><span>{{ date('Y-m-d', $row->created_at->timestamp) }}</span> <span>{{ date('H:i:s', $row->created_at->timestamp) }}</span></time>
+                <div class="cbp_tmicon"></div> {{--cbp_tmicon-earth cbp_tmicon-phone cbp_tmicon-screen cbp_tmicon-mail--}}
+                <div class="cbp_tmlabel">
+                    <h2>{{ round(($row->ended_at - $row->started_at) / 1000, 1) }} 秒, {{ round($row->score) }} 分</h2>
+                    <p>{!! count($row->content) ? $row->content : '<i>备注为空</i>' !!}</p>
                 </div>
-            @endforeach
-        </div>
-    </div>
+            </li>
+        @endforeach
+    </ul>
 @endsection
