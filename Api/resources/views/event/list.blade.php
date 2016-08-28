b19f5a43d6590bdb27699172d31804665aace15d
18601673727 <18601673727@163.com>
2016-08-29 03:51:52 +0800

    timeline added

--- Api/resources/views/event/list.blade.php
+++ Api/resources/views/event/list.blade.php
@@ -10,7 +10,7 @@
                 <div class="cbp_tmicon"></div> {{--cbp_tmicon-earth cbp_tmicon-phone cbp_tmicon-screen cbp_tmicon-mail--}}
                 <div class="cbp_tmlabel">
                     <h2>{{ round(($row->ended_at - $row->started_at) / 1000, 1) }} 秒, {{ round($row->score) }} 分</h2>
-                    <p>{!! count($row->content) ? $row->content : '<i>备注为空</i>' !!}</p>
+                    <p>{!! count(trim($row->content)) ? $row->content : '<i>备注为空</i>' !!}</p>
                 </div>
             </li>
         @endforeach
