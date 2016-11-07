87c165acbb8c709e6803feaac16b925a8107cc59
18601673727 <18601673727@163.com>
2016-11-08 03:03:07 +0800

    debugging backend wechat location logic

--- Api/resources/views/nearby/list.blade.php
+++ Api/resources/views/nearby/list.blade.php
@@ -9,29 +9,29 @@
 @section('script')
     <script src="http://map.qq.com/api/js?v=2.exp&libraries=convertor&key=DNWBZ-7WLAX-IH44W-7B326-A5TPT-YWBU3"></script>
     <script>
-        var center = new qq.maps.LatLng(
+        var dom = document.getElementById('map');
+
+        qq.maps.convertor.translate(new qq.maps.LatLng(
             {{ $location->latitude }},
             {{ $location->longitude }}
-        );
+        ), 1, function(res) {
+            var center = res[0];
 
-        qq.maps.convertor.translate(center, function(newLL) {
-            alert(newLL);
-
-            var map = new qq.maps.Map(document.getElementById('map'), {
-                center: newLL,
+            var map = new qq.maps.Map(dom, {
+                center: center,
                 zoom: 16
             });
 
             var marker = new qq.maps.Marker({
-                position: newLL,
+                position: center,
                 map: map
             });
 
-//        var icon = new qq.maps.MarkerImage(
+            //        var icon = new qq.maps.MarkerImage(
             {{--            {{ session('wechat.oauth_user')->avatar }}--}}
             //        );
 
-//        marker.setIcon(icon);
+            //        marker.setIcon(icon);
 
             marker.setAnimation(qq.maps.MarkerAnimation.DOWN);
         });
