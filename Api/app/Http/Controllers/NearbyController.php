b151e16c40d6bb29799e2721cc864c0e047b5413
18601673727 <18601673727@163.com>
2016-11-09 00:36:31 +0800

    debugging backend wechat location logic

--- Api/app/Http/Controllers/NearbyController.php
+++ Api/app/Http/Controllers/NearbyController.php
@@ -24,7 +24,7 @@ class NearbyController extends Controller
         $mine = Location::where('user_id', auth()->user()->id)->orderBy('created_at', 'desc')->take(1)->first();
 
         // Others location
-        $others = DB::select('SELECT user_id, ( 6371 * acos( cos( radians(:latitude) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(:longitude) ) + sin( radians(:latitude_dupe) ) * sin( radians( latitude ) ) ) ) AS distance FROM locations HAVING distance < :distance WHERE user_id <> :user_id GROUP BY user_id ORDER BY distance LIMIT 0, 20', [
+        $others = DB::select('SELECT user_id, ( 6371 * acos( cos( radians(:latitude) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(:longitude) ) + sin( radians(:latitude_dupe) ) * sin( radians( latitude ) ) ) ) AS distance FROM locations HAVING distance < :distance WHERE user_id != :user_id GROUP BY user_id ORDER BY distance LIMIT 0, 20', [
             'latitude' => $mine->latitude,
             'latitude_dupe' => $mine->latitude,
             'longitude' => $mine->longitude,
